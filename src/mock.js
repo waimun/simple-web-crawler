a63f692f300c94346e57ff46dc7be05ed6ad72ca
waimun <waimun@users.noreply.github.com>
2020-10-29 15:57:50 -0400

    update fetchPage function to return different html snippets based on some fake paths.

--- src/mock.js
+++ src/mock.js
@@ -10,11 +10,114 @@ const htmlSnippet = () => {
   `
 }
 
+const htmlSnippetA = () => {
+  return `
+    <body>
+      <a href="/a1">A1</a>
+      <a href="/a2">A2</a>
+      <a href="/a3">A3</a>
+    </body>
+  `
+}
+
+const htmlSnippetA1 = () => {
+  return `
+    <body>
+      <a href="/a11">A11</a>
+      <a href="/a12">A12</a>
+    </body>
+  `
+}
+
+const htmlSnippetA11 = () => {
+  return `
+    <body>
+      <a href="/a111">A111</a>
+      <a href="/a112">A112</a>
+      <a href="/c">C</a>
+    </body>
+  `
+}
+
+const htmlSnippetA112 = () => {
+  return `
+    <body>
+      <a href="/a1121">A1121</a>
+      <a href="/b">B</a>
+    </body>
+  `
+}
+
+const htmlSnippetB = () => {
+  return `
+    <body>
+      <a href="/b1">B1</a>
+      <a href="/b2">B2</a>
+      <a href="/">/</a>
+    </body>
+  `
+}
+
+const htmlSnippetC = () => {
+  return `
+    <body>
+      <a href="/c1">C1</a>
+      <a href="/a">A</a>
+    </body>
+  `
+}
+
+const htmlSnippetD = () => {
+  return `
+    <body>
+      <p>no links</p>
+    </body>
+  `
+}
+
+const htmlSnippetE = () => {
+  return `
+    <body>
+      <p>no links</p>
+    </body>
+  `
+}
+
 const fetchPage = async (hostname, path = '/') => {
   return new Promise((resolve, reject) => {
     if (hostname === undefined || hostname === null) reject(new Error('missing hostname'))
 
-    resolve({ status: 200, document: htmlSnippet() })
+    switch (path) {
+      case '/':
+        resolve({ status: 200, document: htmlSnippet() })
+        return
+      case '/a':
+        resolve({ status: 200, document: htmlSnippetA() })
+        return
+      case '/a1':
+        resolve({ status: 200, document: htmlSnippetA1() })
+        return
+      case '/a11':
+        resolve({ status: 200, document: htmlSnippetA11() })
+        return
+      case '/a112':
+        resolve({ status: 200, document: htmlSnippetA112() })
+        return
+      case '/b':
+        resolve({ status: 200, document: htmlSnippetB() })
+        return
+      case '/c':
+        resolve({ status: 200, document: htmlSnippetC() })
+        return
+      case '/d':
+        resolve({ status: 200, document: htmlSnippetD() })
+        return
+      case '/e':
+        resolve({ status: 200, document: htmlSnippetE() })
+        return
+      default:
+        resolve({ status: 404, document: '' })
+    }
   })
 }
 
--- src/mock.test.js
+++ src/mock.test.js
@@ -12,7 +12,7 @@ test('parses a html snippet with 5 anchor tags', () => {
   expect(hrefs.length).toEqual(5)
 })
 
-test('mocks fetching a page', async () => {
+test('mocks fetching a page with default root path', async () => {
   const hostname = 'www.google.com'
   const fetchResult = await fetchPage(hostname)
 
@@ -24,6 +24,56 @@ test('mocks fetching a page', async () => {
 
   const hrefs = parseDocumentForAnchorTags(document)
   expect(hrefs.length).toEqual(5)
+})
+
+test('mocks fetching a page with another path', async () => {
+  const hostname = 'www.google.com'
+  const path = '/b'
+
+  const fetchResult = await fetchPage(hostname, path)
+
+  expect(fetchResult).toBeTruthy()
+  expect(fetchResult.status).toEqual(200)
+
+  const document = fetchResult.document
+  expect(document).toBeTruthy()
+
+  const hrefs = parseDocumentForAnchorTags(document)
+  expect(hrefs.length).toEqual(3)
+  expect(hrefs).toContain('/b1')
+  expect(hrefs).toContain('/b2')
+  expect(hrefs).toContain('/')
+})
+
+test('mocks fetching a page that returns no links', async () => {
+  const hostname = 'www.google.com'
+  const path = '/d'
+
+  const fetchResult = await fetchPage(hostname, path)
+
+  expect(fetchResult).toBeTruthy()
+  expect(fetchResult.status).toEqual(200)
+
+  const document = fetchResult.document
+  expect(document).toBeTruthy()
+
+  const hrefs = parseDocumentForAnchorTags(document)
+  expect(hrefs.length).toEqual(0)
+})
+
+test('mocks fetching a 404 page', async () => {
+  const hostname = 'www.google.com'
+  const path = '/notfound'
+
+  const fetchResult = await fetchPage(hostname, path)
+
+  expect(fetchResult).toBeTruthy()
+  expect(fetchResult.status).toEqual(404)
+
+  const document = fetchResult.document
+
+  const hrefs = parseDocumentForAnchorTags(document)
+  expect(hrefs.length).toEqual(0)
 })
 
 test('fetching a page fails with an error', async () => {
