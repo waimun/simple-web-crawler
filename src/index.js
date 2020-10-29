ae8f6961a36da1852320bcb98f1cfbb716626899
waimun <waimun@users.noreply.github.com>
2020-10-29 12:52:42 -0400

    add isInternalLink function.

--- src/index.js
+++ src/index.js
@@ -6,4 +6,13 @@ const parseDocumentForAnchorTags = html => {
   return anchors.map(anchor => anchor.getAttribute('href'))
 }
 
+const isInternalLink = (hostname, path) => {
+  if (hostname === undefined || hostname === null) return false
+  if (path === undefined || path === null) return false
+  if (path.startsWith(`https://${hostname}`)) return true
+  if (path.startsWith('/')) return true
+  return false
+}
+
 exports.parseDocumentForAnchorTags = parseDocumentForAnchorTags
+exports.isInternalLink = isInternalLink
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,20 @@
 const { expect, test } = require('@jest/globals')
+const { isInternalLink } = require('./')
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3)
+test('tests to see if a link is internal', () => {
+  const relativeLink = '/a'
+  const linkWithHostname = 'https://www.google.com'
+  const linkWithAnotherHostname = 'https://www.twitter.com'
+  const hostname = 'www.google.com'
+
+  expect(isInternalLink(hostname, relativeLink)).toBeTruthy()
+  expect(isInternalLink(hostname, linkWithHostname)).toBeTruthy()
+  expect(isInternalLink(hostname, linkWithAnotherHostname)).toBeFalsy()
+  expect(isInternalLink(hostname, linkWithAnotherHostname)).toBeFalsy()
+  expect(isInternalLink(undefined, relativeLink)).toBeFalsy()
+  expect(isInternalLink(null, relativeLink)).toBeFalsy()
+  expect(isInternalLink(hostname, undefined)).toBeFalsy()
+  expect(isInternalLink(hostname, null)).toBeFalsy()
+  expect(isInternalLink()).toBeFalsy()
+  // this test is not extensive or thorough but good enough for the use cases I require
 })
