1cf0174d72ba2d06195b160039ff0fcb2baed9b1
waimun <waimun@users.noreply.github.com>
2020-10-29 21:03:59 -0400

    new fetchPage function to fetch webpage via https.

--- src/index.js
+++ src/index.js
@@ -84,9 +84,38 @@ const crawler = async (hostname, fetcher) => {
   return crawlMap
 }
 
+const fetchPage = async (hostname, path = '/') => {
+  return new Promise((resolve, reject) => {
+    if (hostname === undefined || hostname === null) reject(new Error('missing hostname'))
+
+    const https = require('https')
+    const options = {
+      hostname: hostname,
+      path: path.trim()
+    }
+
+    console.log(`fetching... ${options.path}`)
+
+    const req = https.request(options, (res) => {
+      let content = ''
+
+      res.on('data', d => { content += d.toString() })
+      res.on('end', () => resolve({ status: res.statusCode, document: content }))
+    })
+
+    req.on('error', e => {
+      console.error(`error fetching... ${path}`)
+      reject(e)
+    })
+
+    req.end()
+  })
+}
+
 exports.parseDocumentForAnchorTags = parseDocumentForAnchorTags
 exports.isInternalLink = isInternalLink
 exports.createPage = createPage
 exports.pageFromDocument = pageFromDocument
 exports.fetchAndCreatePage = fetchAndCreatePage
 exports.crawler = crawler
+exports.fetchPage = fetchPage
